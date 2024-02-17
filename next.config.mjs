1ef42b2f347d607da8b4bef3a77daa53ce799d24
AbbasAliKazmi <syedabbasali1998.aa@gmail.com>
2024-02-17 20:11:47 +0500

    Add image remote patterns and update devDependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: "",
+      },
+    ],
+  },
+};
