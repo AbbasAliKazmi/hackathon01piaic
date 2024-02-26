553afba6371e939e6a7d3d1a03bfdcd636d26028
AbbasAliKazmi <syedabbasali1998.aa@gmail.com>
2024-02-26 23:34:53 +0500

    Add sharp dependency and update Sanity project ID

--- sanity/sanity.client.ts
+++ sanity/sanity.client.ts
@@ -1,7 +1,8 @@
 import { createClient, type ClientConfig } from "@sanity/client";
 
 const config: ClientConfig = {
-  projectId: "vvv7t1f3",
+  projectId: "ga8lllhf", 
+    // vvv7t1f3
   dataset: "production",
   apiVersion: "2024-02-14",
   useCdn: false,
